2009d15c23104f68a7738fd4cd0b5d04264c67d2
RomanLesovoy <romadesire97@gmail.com>
2021-03-18 15:17:46 +0200

    Vaults - get pending transactions

---
+++ src/vaults/interfaces/enums.ts
@@ -0,0 +1,73 @@
+export enum VaultTypeTransaction {
+  Withdraw = 'LENDING_REDEMPTION',
+  Deposit = 'LENDING_ALLOCATION',
+  Interest = 'INTEREST',
+  CreditCorrection = 'CREDIT_CORRECTION',
+  DebitCorrection = 'DEBIT_CORRECTION',
+  CreditOther = 'CREDIT_OTHER',
+  DebitOther = 'DEBIT_OTHER',
+}
+
+export enum VaultTransactionStatus {
+  Pending = 'PENDING',
+  Approved = 'APPROVED',
+  Processed = 'PROCESSED',
+  Canceled = 'CANCELLED',
+}
+
+export enum VaultAccountClass {
+  Liability = 'LIABILITY',
+  Equity = 'EQUITY',
+  Asset = 'ASSET ',
+}
+
+export enum VaultTransactionReason {
+  DailyLimit = 'DAILY_LIMIT',
+}
+
+export enum VaultAccountType {
+  Principal = 'LENDING_PRINCIPAL',
+  Interest = 'LENDING_INTEREST',
+  Hold = 'HOLD',
+}
+
+export enum VaultTimeUnit {
+  Second = 'SECOND',
+  Minute = 'MINUTE',
+  Hour = 'HOUR',
+  Day = 'DAY',
+  Week = 'WEEK',
+  Month = 'MONTH',
+  Year = 'YEAR'
+}
+
+export enum VaultTermType {
+  Fixed = 'FIXED',
+  Open = 'OPEN'
+}
+
+export enum VaultAccrualMode {
+  PerBalance = 'PER_BALANCE',
+  PerTransaction = 'PER_TRANSACTION'
+}
+
+export enum VaultAccrualMethod {
+  CompoundInterest = 'COMPOUND_INTEREST',
+  SimpleInterest = 'SIMPLE_INTEREST',
+}
+
+export enum VaultProtocolType {
+  Alkemi = 'ALKEMI'
+}
+
+export enum VaultCategory {
+  DeFi = 'DEFI',
+  Staking = 'STAKING',
+  YieldFarming = 'YIELD_FARMING'
+}
+
+export enum VaultRateType {
+  Fixed = 'FIXED',
+  Stable = 'STABLE',
+  Variable = 'VARIABLE'
+}
---
+++ src/vaults/interfaces/queryParams.ts
@@ -0,0 +1,52 @@
+import * as Enums from "./enums";
+import { TransactionType } from "./index";
+
+export interface VaultPendingTransactionQuery {
+  companyId?: string,
+  userId?: string,
+  productId?: string,
+  assetId?: string,
+  transactionType?: TransactionType,
+  startingCreatedAt?: Date | string,
+  endingCreatedAt?: Date | string,
+  limit?: number,
+  offset?: number,
+  id?: string,
+  status?: Enums.VaultTransactionStatus,
+}
+
+export interface VaultHistoryQuery {
+  companyId?: string,
+  userId?: string,
+  productId?: string,
+  assetId?: string,
+  transactionType?: TransactionType,
+  startingCreatedAt?: Date | string,
+  endingCreatedAt?: Date | string,
+  limit?: number,
+  offset?: number,
+  lendingId?: string,
+  accountType?: Enums.VaultAccountType,
+}
+
+export interface VaultBalancesQuery {
+  accountId?: string,
+  companyId?: string,
+  userId?: string,
+  productId?: string,
+  assetId?: string,
+  accountType?: Enums.VaultAccountType,
+  limit?: number,
+  offset?: number,
+}
+
+export interface VaultProductsQuery {
+  id?: string,
+  companyId?: string,
+  assets?: Array<string>,
+  tags?: Array<string>,
+  enabled?: boolean,
+  termType?: Enums.VaultTermType,
+  protocol?: Enums.VaultProtocolType,
+  category?: Enums.VaultCategory,
+}
