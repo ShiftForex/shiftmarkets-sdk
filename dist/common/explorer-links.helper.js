0dab5deeecedf57378fab15641165cdf77d8f023
RomanLesovoy <roman.lesovoy@shiftforex.com>
2021-07-15 15:22:47 +0300

    Explorer links for currencies

--- dist/common/explorer-links.helper.js
+++ dist/common/explorer-links.helper.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getExplorerLink = exports.explorerLinks = void 0;
 exports.explorerLinks = {
-    xlm: '',
-    eth: '',
-    btc: '',
+    xlm: 'https://stellar.expert/explorer/public',
+    eth: 'https://etherscan.io',
+    btc: 'https://www.blockchain.com/explorer',
 };
 exports.getExplorerLink = (payload) => {
-    const addHash = (link, hash) => !!link ? `${link}${hash}` : '';
+    const addHash = (link, hash) => !!link ? `${link.toLowerCase()}/${hash}` : '';
     const { currency, hash, isErc, } = payload;
     // @ts-ignore
     const explorerLink = isErc ? exports.explorerLinks.eth : exports.explorerLinks[currency];
--- src/common/explorer-links.helper.ts
+++ src/common/explorer-links.helper.ts
@@ -1,7 +1,7 @@
 export const explorerLinks = {
-    xlm: '',
-    eth: '',
-    btc: '',
+    xlm: 'https://stellar.expert/explorer/public',
+    eth: 'https://etherscan.io',
+    btc: 'https://www.blockchain.com/explorer',
 }
 
 export interface ExplorerLinkPayload {
@@ -11,7 +11,7 @@ export interface ExplorerLinkPayload {
 }
 
 export const getExplorerLink = (payload: ExplorerLinkPayload): string => {
-    const addHash = (link: string, hash: string) => !!link ? `${link}${hash}` : '';
+    const addHash = (link: string, hash: string) => !!link ? `${link.toLowerCase()}/${hash}` : '';
     const {
         currency,
         hash,
