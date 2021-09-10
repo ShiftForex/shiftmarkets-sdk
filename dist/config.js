5b068f9e80043d05cfd98a404606e7fa60a8aaaa
Oleksandr Paiziak <paiziak.o@gmail.com>
2021-09-10 11:42:09 +0300

    Updates GEO lookup endpoint

--- dist/config.js
+++ dist/config.js
@@ -14,7 +14,7 @@ exports.default = {
         kyc_api_url: "https://kyc-integration.cryptosrvc.com/api",
         settings_api_url: "https://api.cryptosrvc.com/user-settings",
         lending_api_url: "https://lending-service.cryptosrvc.com/v1",
-        geo_api_url: "https://geo-lookup.cryptosrvc.com",
+        geo_api_url: "https://geo-lookup.cryptosrvc.com/api/v1",
         wyre_api_url: "https://wyre-integration.cryptosrvc.com/v1",
     },
     staging: {
@@ -30,7 +30,7 @@ exports.default = {
         kyc_api_url: "https://kyc-integration-staging.cryptosrvc.com/api",
         settings_api_url: "https://sandbox.shiftmarkets.com/user-settings",
         lending_api_url: "https://lending-service.cryptosrvc-staging.com/v1",
-        geo_api_url: "https://geo-lookup.cryptosrvc-staging.com",
+        geo_api_url: "https://geo-lookup.cryptosrvc-staging.com/api/v1",
         wyre_api_url: "https://wyre-integration.cryptosrvc-staging.com/v1",
     },
 };
